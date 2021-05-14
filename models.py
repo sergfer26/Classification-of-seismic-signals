969914ebe6b03e4199f048d9a5e18c958d92ac1d
Sergio <sfernandezm97@ciencias.unam.mx>
2021-05-14 01:23:18 -0500

    fixed hyperparameters conv layers

--- models.py
+++ models.py
@@ -6,29 +6,28 @@ import torch.nn.functional as F
 ## Defining the architecture of the Encoder.
 class Encoder(nn.Module):
 
-    def __init__(self):
+    def __init__(self, in_channels=3):
         super(Encoder, self).__init__()
 
         ## Convolutional layer
-        self.Conv1 = nn.Conv2d(in_channels=3, out_channels=32, kernel_size=5, stride=2, padding=1)
+        self.Conv1 = nn.Conv2d(in_channels=in_channels, out_channels=32, kernel_size=(12,6), stride=(4, 2), padding=(2, 1))
 
         ## Apply Instance Normalization.
         self.Bn1 = nn.InstanceNorm2d(num_features=32)
 
         ## Convolutional layer
-        self.Conv2 = nn.Conv2d(in_channels=32, out_channels=64, kernel_size=7, stride=2, padding=1)
+        self.Conv2 = nn.Conv2d(in_channels=32, out_channels=64, kernel_size=5, stride=2, padding=1)
 
         ## Apply Instance Normalization.
         self.Bn2 = nn.InstanceNorm2d(num_features=64)
 
         ## Convolutional layer
-        self.Conv3 = nn.Conv2d(in_channels=64, out_channels=128, kernel_size=7, stride=1, padding=1)
+        self.Conv3 = nn.Conv2d(in_channels=64, out_channels=32, kernel_size=4, stride=2, padding=1)
 
         ## Apply Instance Normalization.
-        self.Bn3 = nn.InstanceNorm2d(num_features=128)
-
+        self.Bn3 = nn.InstanceNorm2d(num_features=32)
         ## Fully Connected Layer.
-        self.Fc = nn.Linear(in_features=128 * 4 * 4, out_features=32)
+        self.Fc = nn.Linear(in_features=32 * 62 * 62, out_features=16) # Previously calculated
 
     ## Forward Pass.
     def forward(self, x):
@@ -41,34 +40,34 @@ class Encoder(nn.Module):
 
 class Decoder(nn.Module):
 
-    def __init__(self):
+    def __init__(self, out_channels=3):
         super(Decoder, self).__init__()
 
         ## Fully Connected Layer.
-        self.Fc = nn.Linear(in_features=32, out_features=128 * 4 * 4)
+        self.Fc = nn.Linear(in_features=16, out_features=32 * 62 * 62)
 
         ## Apply Instance Normalization.
-        self.Bn3 = nn.InstanceNorm2d(num_features=128)
+        self.Bn3 = nn.InstanceNorm2d(num_features=32)
 
         ## Convolutional layer
-        self.Conv3 = nn.ConvTranspose2d(in_channels=128, out_channels=64, kernel_size=7, stride=2, padding=1)
+        self.Conv3 = nn.ConvTranspose2d(in_channels=32, out_channels=64, kernel_size=4, stride=2, padding=1)
 
         ## Apply Instance Normalization.
         self.Bn2 = nn.InstanceNorm2d(num_features=64)
         
         ## Convolutional layer
-        self.Conv2 = nn.ConvTranspose2d(in_channels=64, out_channels=32, kernel_size=7, stride=2, padding=1)
+        self.Conv2 = nn.ConvTranspose2d(in_channels=64, out_channels=32, kernel_size=5, stride=2, padding=1)
 
         ## Apply Instance Normalization.
         self.Bn1 = nn.InstanceNorm2d(num_features=32)
 
         ## Convolutional layer
-        self.dConv1 = nn.ConvTranspose2d(in_channels=32, out_channels=3, kernel_size=5, stride=2)
+        self.Conv1 = nn.ConvTranspose2d(in_channels=32, out_channels=out_channels, kernel_size=(12,6), stride=(4, 2), padding=(2, 1))
 
     ## Forward Pass.
     def forward(self, z):
         z = self.Fc(z)
-        z = F.dropout(torch.tanh(self.Bn3(z.view(z.size(0), 128, 4, 4))), p=0.1, training=self.training)
+        z = F.dropout(torch.tanh(self.Bn3(z.view(z.size(0), 32, 62, 62))), p=0.1, training=self.training)
         z = F.dropout(F.leaky_relu(self.Bn2(self.Conv3(z))), p=0.1, training=self.training)
         z = F.dropout(F.leaky_relu(self.Bn1(self.Conv2(z))), p=0.1, training=self.training)
         z = F.leaky_relu(self.Conv1(z))
@@ -78,11 +77,11 @@ class Decoder(nn.Module):
 ## Wrapper class for the entire autoencoder.
 class AutoEncoder(nn.Module):
 
-    def __init__(self):
+    def __init__(self, images=3):
         super(AutoEncoder, self).__init__()
 
-        self.encoder = Encoder()
-        self.decoder = Decoder()
+        self.encoder = Encoder(in_channels=images)
+        self.decoder = Decoder(out_channels=images)
 
     def encode(self, x):
         return self.encoder(x)
